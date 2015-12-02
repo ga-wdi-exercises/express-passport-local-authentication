f301368bb96696dde7a374415ef5f5f955fc2b55
Adam Bray <adam.bray@yale.edu>
2015-12-02 17:41:38 -0500

    fixes bug in login page view

--- config/passport.js
+++ config/passport.js
@@ -17,29 +17,26 @@ module.exports = function(passport) {
     passwordField : 'password',
     passReqToCallback : true
   }, function(req, email, password, callback) {
-    process.nextTick(function() {
+    // Find a user with this e-mail
+    User.findOne({ 'local.email' :  email }, function(err, user) {
+      if (err) return callback(err);
 
-      // Find a user with this e-mail
-      User.findOne({ 'local.email' :  email }, function(err, user) {
-        if (err) return callback(err);
+      // If there already is a user with this email
+      if (user) {
+        return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
+      } else {
+        // There is no email registered with this email
 
-        // If there already is a user with this email
-        if (user) {
-          return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
-        } else {
-          // There is no email registered with this email
+        // Create a new user
+        var newUser            = new User();
+        newUser.local.email    = email;
+        newUser.local.password = newUser.encrypt(password);
 
-          // Create a new user
-          var newUser            = new User();
-          newUser.local.email    = email;
-          newUser.local.password = newUser.encrypt(password);
-
-          newUser.save(function(err) {
-            if (err) throw err;
-            return callback(null, newUser);
-          });
-        }
-      });
+        newUser.save(function(err) {
+          if (err) throw err;
+          return callback(null, newUser);
+        });
+      }
     });
   }));
 
