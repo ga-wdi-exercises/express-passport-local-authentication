93c427fdb1e4cbc48ce61d8db7c5267d4f23c1ea
Jesse Shawl <jesse@jshawl.com>
2015-12-03 10:46:33 -0500

    add passport.use for signup

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+var LocalStrategy = require("passport-local").Strategy
+var User = require("../models/user");
+
+module.exports = function(passport){
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function(req, email, password, callback){
+    User.findOne({ 'local.email' :  email }, function(err, user) {
+      if (err) return callback(err);
+      // If there already is a user with this email
+      if (user) {
+        return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
+      } else {
+        // There is no email registered with this email
+        // Create a new user
+        var newUser            = new User();
+        newUser.local.email    = email;
+        newUser.local.password = newUser.encrypt(password);
+        newUser.save(function(err) {
+          if (err) throw err;
+          return callback(null, newUser);
+        });
+      }
+    });
+  }))
+}
